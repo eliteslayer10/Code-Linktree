e536b4db33c5d5034b2a5ce63b49403577047f95
PRITIPADMA <81458174+PRITIPADMA@users.noreply.github.com>
2021-10-04 09:41:36 +0530

    Firebase-Authentication added
    Slight modification will be done after landing page changes

---
+++ firebase-authentication-main/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDn8O_136xXAvb5c5vljx4jqc94RW7OhPA",
+    authDomain: "login-736cd.firebaseapp.com",
+    projectId: "login-736cd",
+    storageBucket: "login-736cd.appspot.com",
+    messagingSenderId: "633266944367",
+    appId: "1:633266944367:web:540719bf4254cacd94509a",
+    measurementId: "G-L8EP4YPZDB"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
