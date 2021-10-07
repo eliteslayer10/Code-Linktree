1b1f5169bf1659c18d43c25c195cc1db017c8a26
Sai Soumyak Nanda <saisoumyaknanda@gmail.com>
2021-10-07 13:32:20 +0000

    team page added

---
+++ team/team.js
@@ -0,0 +1,50 @@
+const getData = async () => {
+    const api =
+      "https://api.github.com/orgs/The-CODE-Plus-Plus-Community/members";
+    try {
+      let data = await fetch(api);
+      realData = await data.json();
+      // console.log(realData);
+      var html = '<div class="row">';
+      for (i = 0; i < realData.length; i++) {
+        if (i % 3 == 0 && i != 0) {
+          html += "</div>";
+          html += '<div class="row">';
+        }
+        html += `<div class="col-md-4 col-12">`;
+        html += `<div class=" card  mt-5 card_red text-center">
+                                  <div class="title">
+                                  <img src="${realData[i].avatar_url}" alt="" id="imgbg" >
+                                  <div class="mt-3">
+                                  <h4 class="text-light">${realData[i].login}</h4>
+                                  <a href="${realData[i].html_url}">Github</a>
+                                  </div>
+                              </div></div></div>`;
+      }
+  
+      $("#rowdy").append(html);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  getData();
+  function show() {
+    setTimeout(
+      function() {
+        document.getElementById('discord-shoutout').classList.add('online');
+      },
+      200
+    );
+  }
+  
+  function reset() {
+    hide();
+    setTimeout(show, 1500);
+  }
+  
+  function hide() {
+    document.getElementById('discord-shoutout').classList.remove('online');
+  }
+  
+  show();
